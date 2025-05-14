e8abeb2406f75e5be1408df36545b75113d0dc9f
Seongwon Jo <iam@wontory.dev>
2025-05-14 21:28:44 +0900

    feat(web): update Page component layout and integrate orbit visualization

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,39 @@
-import { Button } from '@workspace/ui/components/button'
+import Image from 'next/image'
 
 import { InsetLayout } from '#layouts/inset-layout'
 
 export default function Page() {
   return (
     <InsetLayout title="Home">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="font-bold text-2xl">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+      <h1 className="text-center font-bold text-4xl">
+        <span className="text-center font-bold text-4xl text-primary">
+          tech-stack
+        </span>
+      </h1>
+      <section className="flex flex-col items-center justify-center">
+        <h2 className="text-center font-bold text-2xl">
+          <span className="text-center font-bold text-2xl text-primary">
+            Orbit
+          </span>
+        </h2>
+        <div className="relative aspect-square size-full max-w-4xl">
+          <Image
+            src="/api/orbit?title=hi&text=tech-stack&slugs=react,reactquery,framer,nextdotjs,remix"
+            alt="Orbit"
+            fill={true}
+            draggable={false}
+            unoptimized={true}
+          />
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center">
+        <h2 className="text-center font-bold text-2xl">
+          <span className="text-center font-bold text-2xl text-primary">
+            Badge
+          </span>
+        </h2>
+        <div className="mt-8">Work in progress </div>
+      </section>
     </InsetLayout>
   )
 }
