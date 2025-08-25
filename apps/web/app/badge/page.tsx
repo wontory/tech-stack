148b9b8fada81daaa3f1b5aa48c3422b06d5c26e
조성원 <iam@wontory.dev>
2025-08-26 01:43:55 +0900

    feat(web): update badge page content and layout for improved user experience

--- apps/web/app/badge/page.tsx
+++ apps/web/app/badge/page.tsx
@@ -2,9 +2,9 @@ import { Button } from '@tech-stack/ui/components/button'
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="font-bold text-2xl">Hello World</h1>
-      <Button size="sm">Button</Button>
+    <div className="flex flex-1 flex-col items-center justify-center gap-4">
+      <h1 className="font-bold text-2xl">Work in progress</h1>
+      <Button size="sm">Home</Button>
     </div>
   )
 }
