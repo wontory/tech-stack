093a9b7194242b1c909577c329e2fcaf9871974a
조성원 <iam@wontory.dev>
2025-08-26 01:21:16 +0900

    feat(web): add trash icon to selected icons for removal functionality and enhance button interaction

--- apps/web/src/components/selected-icons.tsx
+++ apps/web/src/components/selected-icons.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import Image from 'next/image'
 import type { SimpleIcon } from 'simple-icons'
 import * as simpleIcons from 'simple-icons/icons'
 
 import { useSlugsState } from '#stores/slugs-context'
+import { TrashIcon } from 'lucide-react'
 
 export function SelectedIcons() {
   const { slugs, setSlugs } = useSlugsState()
@@ -34,12 +36,20 @@ export function SelectedIcons() {
               }}
               // biome-ignore lint/suspicious/noArrayIndexKey: need to use index
               key={`${slug}-${index}`}
-              // biome-ignore lint/security/noDangerouslySetInnerHtml: simple-icons
-              dangerouslySetInnerHTML={{
-                __html: svg.replace('<svg', `<svg fill="#${hex}"`),
-              }}
-              className="cursor-pointer p-2 *:size-6 *:drop-shadow-svg"
-            />
+              className="group relative cursor-pointer p-2"
+            >
+              <div
+                // biome-ignore lint/security/noDangerouslySetInnerHtml: simple-icons
+                dangerouslySetInnerHTML={{
+                  __html: svg.replace('<svg', `<svg fill="#${hex}"`),
+                }}
+                className="size-6 drop-shadow-svg transition group-hover:opacity-50"
+              />
+              <TrashIcon
+                className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 opacity-0 transition group-hover:opacity-80"
+                size={12}
+              />
+            </button>
           )
         })
       ) : (
